c7172193e69c6f3cbee6393a59df870e8f9b5783
Greg Hughes <dcgg_101@hotmail.com>
2020-06-26 19:20:59 -0600

    test2

--- hello.go
+++ hello.go
@@ -8,7 +8,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func main() {
+func hello() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
